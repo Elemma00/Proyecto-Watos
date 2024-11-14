97861ff847343346e2c0725e9388e33c09f29904
Elemma00 <faundez76@gmail.com>
2024-11-13 22:17:22 -0300

    fetching querys to wikidata

---
+++ csv_preprocessing.py
@@ -0,0 +1,25 @@
+import csv
+import re
+
+# Función para procesar caracteres especiales
+def process_special_characters(input_file, output_file):
+    with open(input_file, mode='r', encoding='utf-8') as infile, open(output_file, mode='w', encoding='utf-8', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        
+        for row in reader:
+            processed_row = [col.encode('utf-8').decode('utf-8') for col in row]
+            # delete " character
+            processed_row = [col.replace('"', '') if i == 4 else col for i, col in enumerate(processed_row)]
+            # delete (something) regular expression only for column query
+            processed_row = [re.sub(r'\(.*\)', '', col) if i == 4 else col for i, col in enumerate(processed_row)]
+            writer.writerow(processed_row)
+
+# Archivos de entrada y salida
+input_file = 'trends.csv'
+output_file = 'trends_processed.csv'
+
+# Procesar el archivo CSV
+process_special_characters(input_file, output_file)
+
+print(f"Archivo procesado y guardado como {output_file}")
--- link_rdf_to_wikidata.py
+++ link_rdf_to_wikidata.py
@@ -1,10 +1,10 @@
 import re
 import requests
 
-with open('relations.rdf', 'r', encoding='utf-16') as f:
+with open('trends.rdf', 'r', encoding='utf-8') as f:
     lines = f.readlines()
 
-with open('relations_updated.rdf', 'w', encoding='utf-16') as f:
+with open('trends_updated.rdf', 'w', encoding='utf-8') as f:
     for line in lines:
         f.write(line)
         # if the line contains a query, extract the name of the query
@@ -21,6 +21,10 @@ with open('relations_updated.rdf', 'w', encoding='utf-16') as f:
             }
             response = requests.get(url, params=params)
             data = response.json()
-            concept_uri = data['search'][0]['concepturi']
-            # append a line to link the current entity with the corresponding wikidata entity
-            f.write(f'        ex:wikidata <{concept_uri}> .\n')
+            if data['search']:
+                concept_uri = data['search'][0]['concepturi']
+                # append a line to link the current entity with the corresponding wikidata entity
+                f.write(f'    ex:wikidata <{concept_uri}> ;\n')
+            else:
+                continue
+         
