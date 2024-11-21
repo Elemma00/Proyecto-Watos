294df960430defedc6ab42dddc74cfef2f7897a6
Nicolás Calbucura Villalobos <nicolas.calbucura@ug.uchile.cl>
2024-11-21 17:57:30 -0300

    fix: errors in inferred_trend are solved

--- reasoner.py
+++ reasoner.py
@@ -1,17 +1,29 @@
 from owlrl import DeductiveClosure, OWLRL_Semantics
 import rdflib
 
-
-# Crear un objeto Graph en rdflib
+# Create an rdflib Graph object
 g = rdflib.Graph()
 
-# Cargar tu archivo RDF/OWL
-g.parse("trends.ttl", format="ttl")
+# Load your RDF/OWL files
+g.parse("trends_updated.ttl", format="ttl")
 g.parse("reglas_inferencias.ttl", format="ttl")
 
 closure = DeductiveClosure(OWLRL_Semantics)
 
-# Paso 5: Expansión de inferencias
+# Expand inferences
 closure.expand(g)
-# Mostrar las triples inferidas
+
+# Filter out unwanted inferences
+unwanted_triples = []
+for s, p, o in g:
+    if (p == rdflib.OWL.sameAs and s == o) or (o in [rdflib.XSD.decimal, rdflib.XSD.integer]):
+        unwanted_triples.append((s, p, o))
+    # Check if the subject is not a URI or a blank node
+    if not (isinstance(s, rdflib.URIRef) or isinstance(s, rdflib.BNode)):
+        unwanted_triples.append((s, p, o))
+
+for triple in unwanted_triples:
+    g.remove(triple)
+
+# Serialize the graph with inferred triples
 g.serialize(destination='infered_trend.ttl', format='turtle')
